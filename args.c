a2548752085b1e9eab5d91ef78fab6413e78cc48
rlh-2110 <rolandhartung27@gmx.net>
2024-09-06 07:43:37 +0200

    Changed to c90 standard
    Changed Makefile to use c90
    Added missing semicolons

--- args.c
+++ args.c
@@ -6,8 +6,8 @@ void getArgs(int argc, char*argv[]){
 }
 
 void print_help(char* argv0){
-	printf("Usage: %s [-o <output>] [-h] [-i <inputs>...]\n",argv0)
-	puts("  -o <output>: <output> specifies the name of the output file")
-	puts("  -i <inputs>: <inputs> specifies the input files, can be one or multiple")
-	puts("  -h: shows this help screen")
+	printf("Usage: %s [-o <output>] [-h] [-i <inputs>...]\n",argv0);
+	puts("  -o <output>: <output> specifies the name of the output file");
+	puts("  -i <inputs>: <inputs> specifies the input files, can be one or multiple");
+	puts("  -h: shows this help screen");
 }

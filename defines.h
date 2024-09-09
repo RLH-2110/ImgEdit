3e373e49baa95d1dc0268e96ae8c0b7b602ba05b
RLH-2110 <roland.hartung@gmx.net>
2024-09-09 09:51:19 +0000

    changed binary to hex format

--- defines.h
+++ defines.h
@@ -9,10 +9,9 @@
 #endif
 
 
-/*              0b_---_---_---_---  */
-#define flags_h 0b0000000000000001
-#define flags_o 0b0000000000000010
-#define flags_i 0b0000000000000100
-/*              0b_---_---_---_---  */
+
+#define flags_h 0x01 /* 0b0000_0000_0000_0001 */
+#define flags_o 0x02 /* 0b0000_0000_0000_0010 */
+#define flags_i 0x04 /* 0b0000_0000_0000_0100 */
 
 #endif

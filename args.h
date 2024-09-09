e9969d5d0582b8e0ac279c5a52741c15caa4d019
rlh-2110 <rolandhartung27@gmx.net>
2024-09-09 12:43:49 +0200

    fixed segfault caused by wrong order of operation

--- args.h
+++ args.h
@@ -4,5 +4,11 @@
 /* gets all the arguments and sets the argument flags */
 void getArgs(int argc, char*argv[]);
 
+/* arg = the raw text of the argument, THIS POINER MUST STAY ALIVE FOR THE ENTIRE RUNTIME!
+   list = a pointer to a char** like inputFiles
+   listC = pointer to the count of how many elements are in the list*/
+void appendArg(char *arg, char ***list, int *listC);
+
+
 #endif
 

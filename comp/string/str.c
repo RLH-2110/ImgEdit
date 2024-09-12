984dd0f6620468eba9468e82c8b392045b1fe85b
rlh-2110 <rolandhartung27@gmx.net>
2024-09-12 10:26:31 +0200

    Fixed strcat_c
    Undo change a4eb4f18cf4ffd1225088f2c37f10a6dd24b2c30 'changed **dest -> *dest'
    *dest is changed back to **dest

--- comp/string/str.c
+++ comp/string/str.c
@@ -5,12 +5,12 @@
 
 /* strcat does not seem to work with my DOS compiler, so I make my own.*/
 /* 
-	dest = destination string
+	dest = pointer to destination string
 	src = source string 
 
-	dest will be set to NULL or the new combined string.
+	*dest will be set to NULL or the new combined string.
 */
-void strcat_c(char *dest, const char *src){
+void strcat_c(char **dest, const char *src){
 	int destLen, srcLen, combinedLen;
 
 	if (!dest || !src){ /* If dest or src are null*/
@@ -19,17 +19,17 @@ void strcat_c(char *dest, const char *src){
 	}
 
 
-	destLen = strlen(dest);
+	destLen = strlen(*dest);
 	srcLen = strlen(src);
 	combinedLen = destLen + srcLen + 1; /* add NULL terminator, since strlen does not include one!*/
 
-	dest = realloc(dest,combinedLen); /* make space for both strings */
+	*dest = realloc(*dest,combinedLen); /* make space for both strings */
 
-	if (dest == NULL) /* error handled elsewere */
+	if (*dest == NULL) /* error handled elsewere */
 		return;
 
 
-	strcpy(dest + (destLen),src); /* add src to the end of *dest (and overwerite *dest's null terminator) */
+	strcpy(*dest + (destLen),src); /* add src to the end of *dest (and overwerite *dest's null terminator) */
 }
 
 

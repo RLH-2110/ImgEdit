a4eb4f18cf4ffd1225088f2c37f10a6dd24b2c30
rlh-2110 <rolandhartung27@gmx.net>
2024-09-12 09:27:10 +0200

    Simplified pointer in strcat_c
    changed **dest -> *dest

--- comp/string/str.h
+++ comp/string/str.h
@@ -2,7 +2,7 @@
 #define strH
 
 /* strcat does not seem to work with my DOS compiler, so I make my own.*/
-void strcat_c(char** dest, const char *src);
+void strcat_c(char* dest, const char *src);
 
 /* strH */
 #endif 
